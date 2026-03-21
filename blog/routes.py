43e719df2fcb523b6b77c1745c2ca7e7c264966e
Artur Zieliński <artur.zielinski87@gmail.com>
2026-03-21 18:21:45 +0100

    add in routes.py create_entry

--- blog/routes.py
+++ blog/routes.py
@@ -1,10 +1,28 @@
 # final_project/blog/routes.py
 
-from flask import render_template
+from flask import render_template, request
 from blog import app
-from blog.models import Entry
+from blog.models import Entry, db
+from blog.forms import EntryForm
 
 @app.route("/")
 def index():
     posts = Entry.query.filter_by(is_published=True).all()
     return render_template("index.html", posts=posts)
+
+@app.route("/new-post/", methods=["GET", "POST"])
+def create_entry():
+   form = EntryForm()
+   errors = None
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           entry = Entry(
+               title=form.title.data,
+               body=form.body.data,
+               is_published=form.is_published.data
+           )
+           db.session.add(entry)
+           db.session.commit()
+       else:
+           errors = form.errors
+   return render_template("entry_form.html", form=form, errors=errors)
