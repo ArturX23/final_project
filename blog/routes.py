de8c61104143aad6290a42062e250a7fabb5c121
Artur Zieliński <artur.zielinski87@gmail.com>
2026-03-24 19:25:18 +0100

    add delete_entry

--- blog/routes.py
+++ blog/routes.py
@@ -4,6 +4,7 @@ from flask import render_template, request, session, flash, redirect, url_for
 from blog import app
 from blog.models import Entry, db
 from blog.forms import EntryForm, LoginForm
+from blog.utils import login_required
 
 @app.route("/")
 def index():
@@ -14,6 +15,7 @@ def index():
 
 @app.route("/post/", defaults={'entry_id': None}, methods=["GET", "POST"])
 @app.route("/post/<int:entry_id>", methods=["GET", "POST"])
+@login_required
 def manage_entry(entry_id):
     if entry_id:
         entry = Entry.query.get_or_404(entry_id)
@@ -23,23 +25,23 @@ def manage_entry(entry_id):
         form = EntryForm()
 
     if form.validate_on_submit():
-        if form.validate_on_submit():
-            if entry:
-                form.populate_obj(entry)
-                flash("Wpis został zaktualizowany!")
-            else:
-                entry = Entry(
-                    title=form.title.data,
-                    body=form.body.data,
-                    is_published=form.is_published.data
-                )
-                db.session.add(entry)
-                flash("Twój wpis został dodany!")
+        if entry:
+            form.populate_obj(entry)
+            flash("Wpis został zaktualizowany!")
+        else:
+            entry = Entry(
+                title=form.title.data,
+                body=form.body.data,
+                is_published=form.is_published.data
+            )
+            db.session.add(entry)
+            flash("Twój wpis został dodany!")
 
-            db.session.commit()
-            return redirect(url_for('index'))
+        db.session.commit()
+        return redirect(url_for('index'))
 
     return render_template("entry_form.html", form=form)
+
 
 @app.route("/login/", methods=['GET', 'POST'])
 def login():
@@ -63,3 +65,23 @@ def logout():
        session.clear()
        flash('You are now logged out.', 'success')
    return redirect(url_for('index'))
+
+@app.route("/drafts/")
+@login_required
+def list_drafts():
+    drafts = Entry.query.filter_by(is_published=False)\
+                        .order_by(Entry.pub_date.desc())\
+                        .all()
+    return render_template("drafts.html", drafts=drafts)
+
+@app.route("/delete/<int:entry_id>", methods=["POST"])
+@login_required
+def delete_entry(entry_id):
+    entry = Entry.query.get_or_404(entry_id)
+
+    db.session.delete(entry)
+    db.session.commit()
+
+    flash("Wpis został usunięty!")
+
+    return redirect(url_for('index'))
